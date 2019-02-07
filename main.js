b73b6a2105d75b7957674169d69a3fb9ea0b5209
Meegan Schafer <45988365+schafermeegan@users.noreply.github.com>
2019-02-07 18:43:09 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ let name1=prompt(`Hello, what is the first name?`);
 let name2=prompt(`What is the next name?`);
 let name3=prompt(`What is the final name?`);
 
-if (name1.length > name2.length && name1.length > name3.length) {
+if (name1.length === name2.length && name2.length === name3.length) {
+    console.log(`All three names, ${name1}, ${name2}, and ${name3}, are the same length.`);
+}
+
+else if (name1.length > name2.length && name1.length > name3.length) {
     console.log(`${name1} has the longest name`);
 }
 else if (name2.length > name1.length && name2.length > name3.length) {
@@ -20,6 +24,4 @@ else if (name1.length===name3.length) {
 else if (name2.length===name3.length) {
     console.log(`${name1} and ${name2} tie for the longest name.`);
 }
-else if (name1.length === name2.length && name2.length === name3.length && name1.length === name3.length) {
-    console.log(`All three names, ${name1}, ${name2}, and ${name3}, are the same length.`);
-}
+
